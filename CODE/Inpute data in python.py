12df7dd61dee38b645b36aa88b99b5446f9b6b55
MD BADHON <badhonmolla18@gmail.com>
2023-07-24 01:41:56 +0600

    access

---
+++ CODE/Inpute data in python.py
@@ -0,0 +1,3 @@
+name = input('What is your name: ');
+id = input('What is your ID: ');
+print("My name is " + name + " & My id: " + id)
---
+++ CODE/Vatiable print.py
@@ -0,0 +1,10 @@
+variable = 10
+variable = 30 #python code decode line by line and update
+name = "MD BADHON MOLLAH"
+cgpa = 2.726
+bullion = True
+
+print(variable)
+print(name )
+print(cgpa)
+print(bullion)
